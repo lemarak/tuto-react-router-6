27a0258b403a01261081c1060ec2fe95bbcbed7a
Stéphane Dauguet <sdauguet@gmail.com>
2022-09-06 15:46:21 +0200

    first commit

---
+++ src/routes/invoices.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const invoices = () => {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>Invoices</h2>
+    </main>
+  );
+};
+
+export default invoices;
